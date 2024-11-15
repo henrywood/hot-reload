ff99aa86eb4377d08f561a1581c15cd8156a612d
henrywood <henry.wood.dk@gmail.com>
2024-11-15 20:47:00 +0100

    Update AbstractProcess.php

--- src/AbstractProcess.php
+++ src/AbstractProcess.php
@@ -6,36 +6,25 @@ use EasySwoole\Component\Process\Config;
 
 abstract class AbstractProcess extends Base {
 
-        /**
-         * name  args  false 2 true
-         * AbstractProcess constructor.
-         * @param string $processName
-         * @param null $arg
-         * @param bool $redirectStdinStdout
-         * @param int $pipeType
-         * @param bool $enableCoroutine
-         */
-        function __construct(...$args)
-        {
-                $arg1 = array_shift($args);
-                if($arg1 instanceof Config){
-                        $this->config = $arg1;
-                }else{
-                        $this->config = new Config();
-                        $this->config->setProcessName($arg1);
-                        $arg = array_shift($args);
-                        $this->config->setArg($arg);
-                        $redirectStdinStdout = (bool)array_shift($args) ?: false;
-                        $this->config->setRedirectStdinStdout($redirectStdinStdout);
-                        $pipeType = array_shift($args);
-                        $pipeType = $pipeType === null ? Config::PIPE_TYPE_SOCK_DGRAM : $pipeType;
-                        $this->config->setPipeType($pipeType);
-                        $enableCoroutine = (bool)array_shift($args) ?: false;
-                        $this->config->setEnableCoroutine($enableCoroutine);
-                }
+   protected $swooleProcess;  // Add protected property
 
-                $this->swooleProcess = new \Swoole\Process([$this,'__start'],$this->config->isRedirectStdinStdout(),$this->config->getPipeType(),$this->config->isEnableCoroutine());
-                //$this->swooleProcess = new \Swoole\Process([$this,'__start'],false,0,true);
-                //$this->swooleProcess = new \swoole_process([$this,'__start'],$this->config->isRedirectStdinStdout(),$this->config->getPipeType(),$this->config->isEnableCoroutine());
-        }
+    function __construct(...$args)
+    {
+        $config = new Config();
+        $config->setProcessName('HotReload');
+        $config->setEnableCoroutine(true);
+        
+        parent::__construct($config);
+        
+        $this->swooleProcess = new \Swoole\Process(
+            [$this, '__start'],
+            false,
+            0,
+            true
+        );
+    }
+
+    public function getProcess() {
+        return $this->swooleProcess;
+    }
 }
